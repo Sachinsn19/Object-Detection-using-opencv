b7d2d3f0921dfa865cda5f8a057e462e2954c8e0
Sachin S N <sachinbca214101998@gmail.com>
2022-05-04 10:54:13 +0530

    python file

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template,Response
+import cv2
+
+app = Flask(__name__)
+camera = cv2.VideoCapture(0)
+def generate_frames():
+    while True:
+        success, frame = camera.read() #Read frames
+        if not success:
+            break
+        else:
+            detector = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_frontalface_default.xml')
+            eye_cascades = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_eye.xml')
+            faces = detector.detectMultiScale(frame,1.1,7)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            for x, y, w, h in faces:   #Drawing rectangle around each face
+                cv2.rectangle(frame, (x,y),(x+w, y+h),(255,0,0),2)
+                roi_gray = gray[y:y+h, x:x+w]
+                roi_color = frame[y:y+h, x:x+w]
+                eyes = eye_cascades.detectMultiScale(roi_gray, 1.1,3)
+                for ex, ey, ew, eh in eyes:
+                    cv2.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh),(0,0,255),2)
+
+            ext, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video')
+def video():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
